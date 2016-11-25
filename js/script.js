312b06370f39b22b77f7ba3a868f779cd929513f
dontayj99 <dontayj99@gmail.com>
2016-11-25 18:59:59 -0500

    added the video background

--- js/script.js
+++ js/script.js
@@ -100,6 +100,52 @@ function initAnimations() {
         }
     });
 
+    function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+    }
+
+    function initBannerVideoSize(element){
+
+    $(element).each(function(){
+    $(this).data('height', $(this).height());
+    $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+    }
+
+    function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    console.log(windowHeight);
+
+    $(element).each(function(){
+    var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+    $(this).width(windowWidth);
+
+    if(windowWidth < 1000){
+        videoHeight = windowHeight;
+        videoWidth = videoHeight / videoAspectRatio;
+        $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+        $(this).width(videoWidth).height(videoHeight);
+    }
+
+    $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+    }
+    
     // Service hover animation
     $('.service').hover(function() {
         $('i', this).addClass('animated tada');
@@ -113,6 +159,12 @@ $(document).ready(function() {
     initNavbar();
     initPortfolio();
     initAnimations();
+    scaleVideoContainer();
+
+   initBannerVideoSize('.video-container .poster img');
+   initBannerVideoSize('.video-container .filter');
+   initBannerVideoSize('.video-container video');
+
 });
 
 $(window).load(function() {
@@ -121,4 +173,9 @@ $(window).load(function() {
 
     $loader.find('.fading-line').fadeOut();
     $loader.fadeOut("slow");
+
+    scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
 });
