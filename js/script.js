257f3ae07a3d00bc2df284c987696aa7dce23b1a
dontayj99 <dontayj99@gmail.com>
2016-11-25 23:06:11 -0500

    Fixed home background image

--- js/script.js
+++ js/script.js
@@ -100,59 +100,6 @@ function initAnimations() {
         }
     });
 
-    function scaleVideoContainer() {
-
-        var height = $(window).height() + 5;
-        var unitHeight = parseInt(height) + 'px';
-        $('.homepage-hero-module').css('height', unitHeight);
-
-    }
-
-    function initBannerVideoSize(element) {
-
-        $(element).each(function() {
-            $(this).data('height', $(this).height());
-            $(this).data('width', $(this).width());
-        });
-
-        scaleBannerVideoSize(element);
-
-    }
-
-    function scaleBannerVideoSize(element) {
-
-        var windowWidth = $(window).width(),
-            windowHeight = $(window).height() + 5,
-            videoWidth,
-            videoHeight;
-
-        console.log(windowHeight);
-
-        $(element).each(function() {
-            var videoAspectRatio = $(this).data('height') / $(this).data('width');
-
-            $(this).width(windowWidth);
-
-            if (windowWidth < 1000) {
-                videoHeight = windowHeight;
-                videoWidth = videoHeight / videoAspectRatio;
-                $(this).css({
-                    'margin-top': 0,
-                    'margin-left': -(videoWidth - windowWidth) / 2 + 'px'
-                });
-
-                $(this).width(videoWidth).height(videoHeight);
-            }
-
-            $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
-<<<<<<< HEAD
-  });
-=======
-        });
-    }
->>>>>>> 82357b053bac7f02f5c9c086e10af8e79c89b1c2
-
     // Service hover animation
     $('.service').hover(function() {
         $('i', this).addClass('animated tada');
@@ -168,10 +115,6 @@ $(document).ready(function() {
     initAnimations();
     scaleVideoContainer();
 
-    initBannerVideoSize('.video-container .poster img');
-    initBannerVideoSize('.video-container .filter');
-    initBannerVideoSize('.video-container video');
-
 });
 
 $(window).load(function() {
@@ -181,8 +124,4 @@ $(window).load(function() {
     $loader.find('.fading-line').fadeOut();
     $loader.fadeOut("slow");
 
-    scaleVideoContainer();
-    scaleBannerVideoSize('.video-container .poster img');
-    scaleBannerVideoSize('.video-container .filter');
-    scaleBannerVideoSize('.video-container video');
 });
